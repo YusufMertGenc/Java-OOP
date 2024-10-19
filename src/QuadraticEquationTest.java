753e3da7335b18b9bfac69d39209416cc488b4bd
yusufmert genç <redyusuf@hotmail.com>
2024-10-19 13:13:45 +0300

    Object Oriented Programming with Java

---
+++ src/QuadraticEquationTest.java
@@ -0,0 +1,11 @@
+
+public class QuadraticEquationTest {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+        }
+    }
+}
